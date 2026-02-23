091227dabc75b59e1b5fa1736f17bc6c31e0f92c
PhamThaiHung <phamthaihung9919@gmail.com>
2026-02-23 10:51:59 +0700

    Add files via upload

---
+++ ESP32/ESP32-D0WD-V3/Basic_Wifi_Connect/wifi.h
@@ -0,0 +1,13 @@
+#ifndef WIFI_H
+#define WIFI_H
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <esp_netif.h>
+#include <nvs_flash.h>
+
+#define WIFI_SSID "Nokia 1280"
+#define WIFI_PASSWORD "123451235"
+
+void wifi_initialize(void);
+
+#endif
