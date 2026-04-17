4eaeb3b947f5d10c9437b37da348fe585d705581
PhamThaiHung <phamthaihung9919@gmail.com>
2026-04-17 16:22:43 +0700

    Update main.c

--- STM32/F4/Sensirion/main.c
+++ STM32/F4/Sensirion/main.c
@@ -1,1 +1,27 @@
 
+#include "stdio.h"
+
+void main(void){
+/*
+ *
+ * How to test
+ *
+ * volatile uint16_t pm1;
+  volatile int16_t co2;
+
+ *   Sensor_data sensor;
+  Config(); // GPIOA, C
+  Start_Or_Stop_Sensor(Measure_continuous, 20);
+
+    while (1)
+  {
+	  Return_Value_Of_Sensor(&sensor);
+
+	  pm1 = sensor.pm1; // create more variable
+	  co2 = sensor.co2;
+
+	  delay_ms(20);
+  }
+ */
+
+}
