4a2e6a33f24c6cbf90e267b3fb3bb5eb75b88f6d
PhamThaiHung <phamthaihung9919@gmail.com>
2026-06-12 17:28:03 +0700

    Add files via upload

--- STM32/F4/Drone_Mini/STM32/PWM_Control.c
+++ STM32/F4/Drone_Mini/STM32/PWM_Control.c
@@ -8,6 +8,7 @@
 #include "stdio.h"
 #include "PWM_Control.h"
 #include "bmi088.h"
+#include "cmsis_os.h"
 
 typedef struct {
 	float Kp,Ki,Kd;
@@ -20,10 +21,38 @@ typedef struct {
 	float N;
 }PID_Controller;
 
+PID_Controller Pos_Pitch, Pos_Roll, Pos_Yaw;
 PID_Controller roll_pid,pitch_pid,yaw_pid;
 PID_Controller roll_pid_rate, pitch_pid_rate, yaw_pid_rate;
 
+
 void Setup_PID(void){
+	// Position
+	Pos_Roll.Kp = 1.0f;
+	Pos_Roll.Ki = 0;
+	Pos_Roll.Kd = 0.005f;
+	Pos_Roll.N = 50.0f;
+
+	Pos_Roll.integral = 0;
+	Pos_Roll.previous_error = 0;
+
+	Pos_Pitch.Kp = 1.0f;
+	Pos_Pitch.Ki = 0;
+	Pos_Pitch.Kd = 0.005f;
+	Pos_Pitch.N = 50.0f;
+
+	Pos_Pitch.integral = 0;
+	Pos_Pitch.previous_error = 0;
+
+	Pos_Yaw.Kp = 1.0f;
+	Pos_Yaw.Ki = 0;
+	Pos_Yaw.Kd = 0.005f;
+	Pos_Yaw.N = 50.0f;
+
+	Pos_Yaw.integral = 0;
+	Pos_Yaw.previous_error = 0;
+
+
 	// dt = 1/200f
 	roll_pid.Kp = 1.0f;
 	roll_pid.Ki = 0;
@@ -50,7 +79,7 @@ void Setup_PID(void){
 	yaw_pid.integral = 0;
 	yaw_pid.previous_error = 0;
 
-	float limit = 480.0f;
+	float limit = 3000.0f;
 	roll_pid.output_lim = limit;
 	pitch_pid.output_lim = limit;
 	yaw_pid.output_lim = limit;
@@ -87,31 +116,7 @@ void Setup_PID(void){
 
 }
 
-float Update_PID_Angle(PID_Controller *pid, float measure_value,float dt){
-	float error_Angle = pid->sp - measure_value;
-	// Cal P
-	float P = pid->Kp * error_Angle;
-	// Cal I and Anti-Windup
-	pid->integral += error_Angle*dt;
-	if(pid->integral > 40) pid->integral = 40; // saturation
-	else if(pid->integral < -40) pid->integral = -40;
-
-	float I = pid->Ki * pid->integral;
-	// Cal D
-	float derivative = (error_Angle - pid->previous_error) / dt;
-	float D = pid->Kd * derivative;
-	pid->d_filter += (D - pid->d_filter)*(pid->N * dt);
-
-	float output = P + I + pid->d_filter;
-
-	if(output > pid->output_lim) output = pid->output_lim;
-	if(output < -pid->output_lim) output = - pid->output_lim;
-
-	pid->previous_error = error_Angle;
-	return output;
-}
-
-float Update_PID_Rate(PID_Controller *pid, float measure_value,float dt){ // velocity angle
+float Update_PID(PID_Controller *pid, float measure_value,float dt){ // velocity angle
 	float error_Rate = pid->sp - measure_value;
 	// Cal P
 	float P = pid->Kp * error_Rate;
@@ -141,29 +146,47 @@ float Update_PID_Rate(PID_Controller *pid, float measure_value,float dt){ // vel
 // M1 (CW) rear , M4 (CCW) rear
 volatile float u_roll = 0,u_pitch = 0,u_yaw = 0;
 volatile float u_roll_rate = 0, u_pitch_rate = 0, u_yaw_rate = 0;
+volatile float u_roll_pos = 0,u_pitch_pos = 0,u_yaw_pos = 0;
 int16_t M1 = 0,M2 = 0,M3 = 0,M4 = 0;
+
 float base_pwm = 1680.0f; // 20%
+
 float dt = 0.005f;
+int x_pos = 0, y_pos = 0;
+
+extern osThreadId_t Motor_Thread;
+
+//float Pos_Cam = 0; Angle_Cam = 0;
+//float Cam_hz = 0.03333f;
 
 void Control_PWM(void){
+	// Position Loop
+//	u_roll_pos = Update_PID(&Pos_Roll, Pos_Cam, Cam_hz);
+
 	// The outer circle - The angle of IMU
-	u_roll_rate = Update_PID_Angle(&roll_pid, drone_angle.Roll_angle, dt);
-	u_pitch_rate = Update_PID_Angle(&pitch_pid, drone_angle.Pitch_angle,dt);
-	u_yaw_rate = Update_PID_Angle(&yaw_pid, drone_angle.Yaw_angle, dt);
+	u_roll_rate = Update_PID(&roll_pid, drone_angle.Roll_angle, dt);
+	u_pitch_rate = Update_PID(&pitch_pid, drone_angle.Pitch_angle,dt);
+	u_yaw_rate = Update_PID(&yaw_pid, drone_angle.Yaw_angle, dt);
 
 	// The inner circle
 	roll_pid_rate.sp = u_roll_rate;
 	pitch_pid_rate.sp = u_pitch_rate;
 	yaw_pid_rate.sp = u_yaw_rate;
 
-	u_roll = Update_PID_Rate(&roll_pid_rate, final.gx,dt);
-	u_pitch = Update_PID_Rate(&pitch_pid_rate, final.gy, dt);
-	u_yaw = Update_PID_Rate(&yaw_pid_rate, final.gz, dt);
+	u_roll = Update_PID(&roll_pid_rate, final.gx, dt);
+	u_pitch = Update_PID(&pitch_pid_rate, final.gy, dt);
+	u_yaw = Update_PID(&yaw_pid_rate, final.gz, dt);
 
-	M1 = (int16_t)(base_pwm - u_roll + u_pitch - u_yaw); // M1 (CW) rear - right
-	M2 = (int16_t)(base_pwm + u_roll + u_pitch + u_yaw); // M2 (CCW) rear - left
-	M3 = (int16_t)(base_pwm + u_roll - u_pitch - u_yaw); // M3 (CW) front - left
-	M4 = (int16_t)(base_pwm - u_roll - u_pitch + u_yaw); // M4 (CCW) front - right
+	if(fabs(u_roll) > 30 || fabs(u_pitch) > 30){
+		M1 = 0,M2 = 0,M3 = 0,M4 = 0;
+		osThreadFlagsSet(Motor_Thread, 2);
+	}
+	else {
+		M1 = (int16_t)(base_pwm - u_roll + u_pitch - u_yaw); // M1 (CW) rear - right
+		M2 = (int16_t)(base_pwm + u_roll + u_pitch + u_yaw); // M2 (CCW) rear - left
+		M3 = (int16_t)(base_pwm + u_roll - u_pitch - u_yaw); // M3 (CW) front - left
+		M4 = (int16_t)(base_pwm - u_roll - u_pitch + u_yaw); // M4 (CCW) front - right
+	}
 
 	// ARR = 3360 = 100%
 	TIM2->CCR4 = (M1 > 2688) ? 2688 : (M1 < 0 ? 0 :(uint32_t)M1); // 80%
--- STM32/F4/Drone_Mini/STM32/Radio_Communication.c
+++ STM32/F4/Drone_Mini/STM32/Radio_Communication.c
@@ -9,41 +9,79 @@
 
 void USART_Configuration(void){
 	RCC->APB2ENR |= (1 << 5); // UART 6
+	RCC->AHB1ENR |= (1 << 2); // GPIOC
 	for(volatile int i = 0; i < 100; i++); // wait for stable
 
-	// USART 6 PC6_TX, PC7_RX 24MHz
+	// USART 6 PC6_TX, PC7_RX 84MHz
 	GPIOC->MODER &= ~(3 << 12) &~(3 << 14);
 	GPIOC->MODER |= (2 << 12)|(2 << 14);
+
 	GPIOC->OSPEEDR |= (3 << 12)|(3 << 14);
 	GPIOC->AFR[0] &= ~(0x0F << 24) &~(0x0F << 28);
-	GPIOC->AFR[0] |= (8 << 24)|(8 << 28);
+	GPIOC->AFR[0] |= (8 << 24)|(8 << 28); // AF8
 
 	USART6->CR1 &= ~(1 << 13);
-	USART6->BRR = (156 << 4)|(4 << 0); // 9600
+	USART6->BRR = (5 << 4)|(11 << 0); // 921600
 	USART6->CR1 |= (1 << 2)|(1 << 3)|(1 << 5)|(1 << 13);
+	NVIC_SetPriority(USART6_IRQn, 5);
 	NVIC->ISER[2] |= (1 << 7);
 }
 
-int Drone_State = 0,idx = 0;
-char Data[] = {0};
-extern osThreadId_t Radio_Thread;
+void USART6_Send_Char(char c){
+	while((USART6->SR & (1 << 6)) == 0){};
+	USART6->DR = c;
+}
 
+void USART6_Send_String(char *s){
+	while(*s){
+		USART6_Send_Char(*s++);
+	}
+}
+
+int converted(char *buffer){
+	char check[11] = {'0','1','2','3','4','5','6','7','8','9'};
+	char temp[6];
+	int k = 0;
+	for(int i = 0; i < 5; i++){
+		for(int j = 0; j < 10; j++){
+			if(buffer[i] == check[j]){
+				temp[k++] = buffer[i];
+				break;
+			}
+		}
+	if(buffer[i] == '\0') break;
+    }
+	return atoi(temp); // convert char to integer
+}
+
+extern osThreadId_t Radio_Thread;
+extern osMessageQueueId_t radioQueueHandle;
 void USART6_IRQHandler(void) {
-    if (USART6->SR & (1 << 5)) { // Kiểm tra cờ RXNE (có dữ liệu đến)
-        Data[idx++] = USART6->DR;
-        // Xử lý dữ liệu ở đây
-        if(strcmp(Data,"Radio_Ready") == 0){
-        	osThreadFlagsSet(Radio_Thread, 0x0001);
+
+    uint32_t sr = USART6->SR;
+    if (sr & (1 << 3)) {
+        volatile uint32_t dummy = USART6->DR; // Đọc DR để ép xóa cờ lỗi ORE vĩnh viễn
+        (void)dummy;
+    }
+    if (sr & (1 << 5)) {
+        char rx_byte = (char)USART6->DR;
+    	static char rx_buffer[RADIO_MSG_LEN];
+		static int idx = 0;
+        if (rx_byte == '\n' || rx_byte == '\r') {
+            if (idx > 0) {
+            	rx_buffer[idx] = '\0';
+                osMessageQueuePut(radioQueueHandle, rx_buffer, 0, 0);
+                idx = 0;
+            }
         }
-        if(strcmp(Data,"Move_Forward") == 0){
-        	// Do something
-        	idx = 0;
-        	memset(Data,0,sizeof(Data));
-        }
-        if(strcmp(Data,"Move_back") == 0){
-        	// Do something
-        	idx = 0;
-        	memset(Data,0,sizeof(Data));
+        else {
+            if (idx < 31) {
+                if (rx_byte >= 32 && rx_byte <= 126) {
+                	rx_buffer[idx++] = rx_byte;
+                }
+            } else {
+                idx = 0;
+            }
         }
     }
 }
