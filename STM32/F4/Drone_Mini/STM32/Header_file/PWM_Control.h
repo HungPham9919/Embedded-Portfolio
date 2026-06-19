4db062d4468c683ea17397bb86c8a342c550f320
PhamThaiHung <phamthaihung9919@gmail.com>
2026-06-19 12:23:29 +0700

    Add files via upload

--- STM32/F4/Drone_Mini/STM32/Header_file/PWM_Control.h
+++ STM32/F4/Drone_Mini/STM32/Header_file/PWM_Control.h
@@ -12,12 +12,6 @@ extern volatile float u_roll,u_pitch,u_yaw ;
 extern int16_t M1,M2,M3,M4;
 void Setup_PID(void);
 
-void Control_PWM(void);
-void Move_Right(void);
-void Move_Left(void);
-void Rotate_CCW(void);
-void Rotate_CW(void);
-void Move_Back(void);
-void Move_Forward(void);
+void Control_PWM(float roll_sp, float pitch_sp, float yaw_sp);
 
 #endif /* INC_PWM_CONTROL_H_ */
